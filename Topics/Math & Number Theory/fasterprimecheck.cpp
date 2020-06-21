0f6079010a471c9ac11fcc777732395cc85cd131
Ahmed Hafez <ahmedtronic@gmail.com>
2020-06-21 18:13:06 +0200

    add a function for fast prime checking

---
+++ Topics/Math & Number Theory/fasterprimecheck.cpp
@@ -0,0 +1,15 @@
+bool isprime(ll n)
+{
+	if (n <= 1)  return false;
+	if (n <= 3)  return true;
+
+	// This is checked so that we can skip  
+	// middle five numbers in below loop 
+	if (n % 2 == 0 || n % 3 == 0) return false;
+
+	for (ll i = 5; i*i <= n; i = i + 6)
+		if (n%i == 0 || n % (i + 2) == 0)
+			return false;
+
+	return true;
+}
